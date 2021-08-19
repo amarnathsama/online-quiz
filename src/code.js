867b06d99d774286c750dd0d4e4a68905c5955b9
amarnathsama <amar22111999@gmail.com>
2021-08-19 13:29:16 +0530

    first commit

--- src/code.js
+++ src/code.js
@@ -1,15 +1,14 @@
-import React from 'react';
-import Header from './header'
+import React from "react";
+import Header from "./header";
 var Code = React.createClass({
-  render() {
-    return (
-      <div>
-      <Header/>
-        Hey this is Code Challenge by sapient.
-
-      </div>
-    );
-  }
-})
+    render() {
+        return (
+            <div>
+                <Header />
+                This is an online quiz system.
+            </div>
+        );
+    },
+});
 
 export default Code;
